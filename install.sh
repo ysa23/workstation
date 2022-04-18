5f6d6f01ac9c6bb4010acb222141256e9c7b5624
Yossi Shmueli <ysa.angel@gmail.com>
2022-04-18 19:04:42 +0300

    chore(install): add support for parameters

--- install.sh
+++ install.sh
@@ -1,13 +1,81 @@
-GIT_USER_NAME=GITHUB_USER_NAME
-GIT_EMAIL=GITHUB_EMAIL
-GITHUB_PACKAGES_TOKEN=TOKEN # settings => developer settings => personal access tokens
+POSITIONAL_ARGS=()
 
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -u|--git-user-name)
+      GIT_USER_NAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -e|--git-email)
+      GIT_EMAIL="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -t|--github-packages-token)
+      GITHUB_PACKAGES_TOKEN="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -x|--exclude)
+      IFS=',' read -r -a EXCLUDE <<< "$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+usage (){
+  echo "Usage: ./workstation.sh -u|--git-user-name <YOUR-GIT-USER-NAME> -e|--git-email <YOUR-GIT-EMAIL> -t|--github-packages-token <GITHUB-PACKAGES-TOKEN> [-x|--exclude <comma separated string for apps that will not be installed>]"
+  echo "Usage: Github packages token can be fetched from: github.com => settings => developer settings => personal access tokens"
+  echo "Usage: Available excludes: whatsapp,telegram,visual-studio-code,github,gh,notion,ngrok,goland,awscli,gcloud,sublime-text,go,mongo,tunnelblick,openvpn-connect,k8s,bzt,terraform"
+}
+
+if [ -z "${GIT_USER_NAME}" ] || [ -z "${GIT_EMAIL}" ] || [ -z "${GITHUB_PACKAGES_TOKEN}" ]; then
+  usage
+  exit 1
+fi
+
+arrayContains (){
+  declare -a array=("${!1}")
+  local match=$2
+
+  for item in "${array[@]}"
+  do
+    if [[ "$item" == "$match" ]]; then
+      return 1
+    fi
+  done
+
+  return 0
+}
+
+install (){
+  local name=$1
+  local options=$2
+  arrayContains EXCLUDE[@] $name
+  if [[ "$?" == "0" ]]; then
+    brew install ${options} $name
+  fi
+}
 
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+0>/dev/null sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
@@ -16,25 +84,51 @@ rm -rf fonts
 chmod 755 /usr/local/share/zsh
 chmod 755 /usr/local/share/zsh/site-functions
 
-echo "Make sure to edit .zshrc with t"
-
 # Applications
-brew install --cask iterm2
-brew install --cask webstorm
-brew install --cask goland
-brew install --cask datagrip
-brew install --cask visual-studio-code
-brew install --cask postman
-brew install --cask telegram
-brew install --cask sublime-text
-brew install --cask dashlane
-brew install --cask slack
+install notion --cask 
+install iterm --cask
 
-# go
-brew install go
-zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-git config --global url.git@github.com:.insteadOf https://github.com
-echo "export GOPRIVATE=\"github.com/climacell/*\"" >> ~/.zshrc
+# Dev Apps
+brew install --cask webstorm
+brew install --cask pycharm
+brew install --cask postman
+brew install --cask datagrip
+install goland --cask 
+install ngrok --cask 
+install github --cask
+
+# Cloud CLIs
+install awscli
+arrayContains EXCLUDE[@] gcloud
+if [[ "$?" == "0" ]]; then
+  brew install --cask google-cloud-sdk
+  gcloud init
+  source \"/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc\"
+  source \"/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc\"
+fi
+arrayContains EXCLUDE[@] heroku
+if [[ "$?" == "0" ]]; then
+  brew tap heroku/brew && brew install heroku
+fi
+
+# Text editors
+install atom --cask
+install sublime-text --cask 
+install visual-studio-code --cask
+
+# Communication
+install whatsapp --cask
+install telegram --cask
+brew install --cask slack
+brew install --cask zoom
+
+# Mongo
+arrayContains EXCLUDE[@] mongo
+if [[ "$?" == "0" ]]; then
+  brew install --cask mongodb-compass
+  brew tap mongodb/brew
+  brew install mongodb-database-tools
+fi
 
 # Node
 brew install nvm
@@ -42,21 +136,20 @@ brew install yarn
 echo "//npm.pkg.github.com/:_authToken=$GITHUB_PACKAGES_TOKEN" > ~/.npmrc
 nvm install 12
 nvm install 16
+export NVM_DIR=\"$HOME/.nvm\"
+[ -s \"/opt/homebrew/opt/nvm/nvm.sh\" ] && \. \"/opt/homebrew/opt/nvm/nvm.sh\"  # This loads nvm
+[ -s \"/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm\" ] && \. \"/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm\"  # This loads nvm bash_completion
+" >> ~/.zshrc
 
-# VPN
-brew install --cask tunnelblick
-brew install --cask openvpn-connect
-
-# Gcloud
-brew install --cask google-cloud-sdk
-gcloud init
-
-# K8s
-brew install kubectl
-brew install kubectx
-brew install k9s
-brew install fzf
-brew install --cask lens
+# go
+arrayContains EXCLUDE[@] go
+if [[ "$?" == "0" ]]; then
+  brew install go
+  zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+  git config --global url.git@github.com:.insteadOf https://github.com
+  echo "export GOPRIVATE=\"github.com/climacell/*\"" >> ~/.zshrc
+  [[ -s \"\$HOME/.gvm/scripts/gvm\" ]] && source \"\$HOME/.gvm/scripts/gvm\"" >> ~/.zshrc
+fi
 
 # docker
 brew install --cask docker
@@ -65,21 +158,35 @@ brew install --cask docker
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_EMAIL"
 
-#github
-brew install gh
-
-# SSH
-ssh-keygen -t rsa -b 4096 -C "$GIT_EMAIL"
-
 # P4merge
 brew install --cask p4v
 git config --global merge.tool p4mergetool
 git config --global mergetool.p4mergetool.cmd 
-git config --global mergetool.p4mergetool.cmd "/usr/local/Caskroom/p4merge.app/Contents/Resources/launchp4merge \$PWD/\$BASE \$PWD/\$REMOTE \$PWD/\$LOCAL \$PWD/\$MERGED"
+git config --global mergetool.p4mergetool.cmd "/Applications/p4merge.app/Contents/MacOS/p4merge \"$BASE\" \"$LOCAL\" \"$REMOTE\" \"$MERGED\""
 git config --global mergetool.p4mergetool.trustExitCode false
 git config --global mergetool.keepBackup false
 git config --global diff.tool p4mergetool
-git config --global difftool.p4mergetool.cmd "/usr/local/Caskroom/p4merge.app/Contents/Resources/launchp4merge \$LOCAL \$REMOTE"
+git config --global difftool.p4mergetool.cmd "/Applications/p4merge.app/Contents/MacOS/p4merge \"$LOCAL\" \"$REMOTE\""
+
+# VPN
+install tunnelblick --cask 
+install openvpn-connect --cask 
+
+# K8s
+arrayContains EXCLUDE[@] k8s
+if [[ "$?" == "0" ]]; then
+  brew install kubectl
+  brew install kubectx
+  brew install k9s
+  brew install fzf
+  brew install --cask lens
+fi
+
+#github
+install gh
+
+# SSH
+ssh-keygen -t rsa -b 4096 -C "$GIT_EMAIL"
 
 #Configurations
 # echo "prompt_context() {
@@ -88,25 +195,23 @@ git config --global difftool.p4mergetool.cmd "/usr/local/Caskroom/p4merge.app/Co
 #   fi
 # }
 
-echo "# Default editor
-export EDITOR='subl -w'
-
-# NVM
-export NVM_DIR=\"\$HOME/.nvm\"
-[ -s \"/usr/local/opt/nvm/nvm.sh\" ] && . \"/usr/local/opt/nvm/nvm.sh\"  # This loads nvm
-[ -s \"/usr/local/opt/nvm/etc/bash_completion.d/nvm\" ] && . \"/usr/local/opt/nvm/etc/bash_completion.d/nvm\"  # This loads nvm bash_completion
-
-# Gcloud
-source \"/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc\"
-source \"/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc\"
+# Default editor
+arrayContains EXCLUDE[@] sublime-text
+if [[ "$?" != "0" ]]; then
+  echo "# Default editor
+  export EDITOR='subl -w'  
+fi
 
 # Blazemeter
-brew install bzt
-
-[[ -s \"\$HOME/.gvm/scripts/gvm\" ]] && source \"\$HOME/.gvm/scripts/gvm\"" >> ~/.zshrc
+install bzt
 
 # Folders
 mkdir ~/dev
 
 # terraform
-brew install warrensbox/tap/tfswitch
+arrayContains EXCLUDE[@] terraform
+if [[ "$?" == "0" ]]; then
+  brew install warrensbox/tap/tfswitch
+fi
+
+echo "Make sure to edit .zshrc with a theme you like"
